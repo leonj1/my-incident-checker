8f1df7fc84bb26cc7a15a14d1e93d2a1c0fb44dc
Jose Leon <leonj1@gmail.com>
2025-02-17 02:35:05 +0000

    FIX Missing Critical Function Implementation

--- lights/utils.go
+++ lights/utils.go
@@ -1,9 +1,16 @@
 package lights
 
-import "github.com/tarm/serial"
+import (
+	"fmt"
+
+	"github.com/tarm/serial"
+)
 
 // sendCommand sends a command byte to the serial port
 func sendCommand(port *serial.Port, cmd byte) error {
 	_, err := port.Write([]byte{cmd})
-	return err
+	if err != nil {
+		return fmt.Errorf("failed to send command: %w", err)
+	}
+	return nil
 }
