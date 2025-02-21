2e7c3b0f39a232ea89a0e7f4be3678f93a549ddc
Jose Leon <leonj1@gmail.com>
2025-02-21 02:25:22 +0000

    FIX Inconsistent error handling in Close()

--- lights/serial_light_noserial.go
+++ lights/serial_light_noserial.go
@@ -7,23 +7,26 @@ import "fmt"
 // SerialLight implements Light interface for serial-based tower lights
 type SerialLight struct{}
 
+// Error message constant for unavailable serial support
+const errNoSerialSupport = "serial port support not available in this build"
+
 // NewSerialLight creates a new SerialLight instance
 func NewSerialLight(port string, baudRate int) (*SerialLight, error) {
-	return nil, fmt.Errorf("serial port support not available in this build")
+	return nil, fmt.Errorf(errNoSerialSupport)
 }
 
 func (l *SerialLight) On(cmd interface{}) error {
-	return fmt.Errorf("serial port support not available in this build")
+	return fmt.Errorf(errNoSerialSupport)
 }
 
 func (l *SerialLight) Clear() error {
-	return fmt.Errorf("serial port support not available in this build")
+	return fmt.Errorf(errNoSerialSupport)
 }
 
 func (l *SerialLight) Blink(cmd interface{}) error {
-	return fmt.Errorf("serial port support not available in this build")
+	return fmt.Errorf(errNoSerialSupport)
 }
 
 func (l *SerialLight) Close() error {
-	return nil
+	return fmt.Errorf(errNoSerialSupport)
 }
